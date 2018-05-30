b437b82e0172fc3e1d5436d0846dbbc99f4bff4f
Filip Petrovic <roll4@outlook.com>
2018-05-31 01:43:13 +0200

    Transfer

--- app/src/main/java/com/filipetrovic/auxilium/MainActivity.kt
+++ app/src/main/java/com/filipetrovic/auxilium/MainActivity.kt
@@ -11,8 +11,8 @@ import android.os.Handler
 import android.os.Looper
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
-import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 private var sampleRate: Int = 0
 private var bufferSize: Int = 0
@@ -29,8 +29,16 @@ private var handler: Handler? = null
 private var thread: Thread? = null
 private var comp: Float? = 0f
 private var counter: Int? = 0
+private var frequencyIndex = 0
 
 class MainActivity : AppCompatActivity() {
+
+    var FREQUENCIES = doubleArrayOf(65.41, 69.30, 73.42, 77.78, 82.41, 87.31, 92.50, 98.00, 103.83, 110.00, 116.54, 123.47, 130.81, 138.59, 146.83, 155.56, 164.81, 174.61, 185.00, 196.00, 207.65, 220.00, 233.08, 246.94, 261.63, 277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392.00, 415.30, 440.00, 466.16, 493.88, 523.25, 554.37, 587.33, 622.25, 659.25, 698.46, 739.99, 783.99, 830.61, 880.00, 932.33, 987.77)
+
+    var  NOTE_NAMES = arrayOf("C","C#","D","D#","E","F","F#","G","G#","A","A#","B")
+
+//    var NOTE_NAMES = arrayOf("C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B")
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,6 +52,21 @@ class MainActivity : AppCompatActivity() {
         }
         init()
         start()
+    }
+
+
+    private fun findFrequency(freq: Double): String {
+        var index = -1
+        var dist = java.lang.Double.MAX_VALUE
+        for (i in 0 until FREQUENCIES.size) {
+            val d = Math.abs(freq - FREQUENCIES[i])
+            if (d < dist) {
+                index = i
+                dist = d
+            }
+        }
+        frequencyIndex = index;
+        return NOTE_NAMES[index % 12] + "" + (index / 12 + 2)
     }
 
 
@@ -91,7 +114,26 @@ class MainActivity : AppCompatActivity() {
 //                    mainTextview.setText(comp / 5 + "")
 //                    comp = 0f
 //                }
-                textView.text = currentNote!!.getActualFrequency().toString()
+
+                var freq = currentNote!!.getActualFrequency();
+                freq = Math.round(freq * 100.0) / 100.0
+                tvNote.text = findFrequency(freq);
+                tvFrequency.text = freq.toString()
+
+                if(frequencyIndex != 0 && frequencyIndex < FREQUENCIES.size - 1 && freq != FREQUENCIES[frequencyIndex]) {
+                    var nextNote: Double = 0.0
+                    when(freq > FREQUENCIES[frequencyIndex]) {
+                        true -> nextNote = FREQUENCIES[frequencyIndex + 1]
+                        false -> nextNote = FREQUENCIES[frequencyIndex - 2]
+                    }
+
+                    var percentage: Double = ((freq - FREQUENCIES[frequencyIndex]) / (nextNote - FREQUENCIES[frequencyIndex])) * 100
+                    percentage = Math.round(percentage * 100.0) / 100.0
+//                    Log.d("Percentage Aux", freq.toString() + " " + currentNote!!.frequency + " " + nextNote);
+//                    percentage = Math.round(percentage);
+                    textViewpercentage.text = "${percentage} away from  ${tvNote.text}";
+                }
+
             })
         }
     }
